49cc6175d579d999b461300872d9e1f1d6103367
bapatakshay1 <54824917+bapatakshay1@users.noreply.github.com>
2021-03-01 14:11:37 -0500

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -23,8 +23,8 @@ def topCrimeYTD(data, handler): #YearToDate function for each precinct
   top5precinctYear.drop(columns=unwanted_Cols, inplace=True)
   top5precinctYear['top5CrimeByPrec_YTD'] = top5precinctYear.max(axis=1)
   top5precinctYear['Max'] = top5precinctYear.idxmax(axis=1)
-  top5precinctYear[['top5CrimeByPrec_YTD', 'Max']].sort_values(by=['top5CrimeByPrec_YTD'], ascending=False).head(5)
-  return data
+  top5precinctYear=top5precinctYear[['top5CrimeByPrec_YTD', 'Max']].sort_values(by=['top5CrimeByPrec_YTD'], ascending=False)
+  return top5precinctYear
 
 
 def topCrimeWTD(data, handler):#WeekToDate function for each precinct
@@ -52,10 +52,11 @@ def topCrimeMTD(data, handler):#MonthToDate function for each precinct
   top5precinct=top5precinct[['top5CrimeByPrecMTD', 'Max']].sort_values(by=['top5CrimeByPrecMTD'], ascending=False)
   return top5precinct
 
-
-# TopCrimeYTD-data:
-  #   pattern: /$YAMLURL/TopCrimeYTD
-  #   handler: FormHandler
-  #   kwargs:
-  #     url: sqlite:///$YAMLPATH/save_pandas.db
-  #     table: TopCrimes
+thisdict = {'40':"Bronx",'41':"Bronx",'42':"Bronx",'43':"Bronx",'44':"Bronx",'45':"Bronx",'46':"Bronx",'47':"Bronx",'48':"Bronx",'49':"Bronx",'50':"Bronx",'52':"Bronx",
+  '60':"PB Brooklyn South",'61':"PB Brooklyn South",'62':"PB Brooklyn South",'63':"PB Brooklyn South",'66':"PB Brooklyn South",'67':"PB Brooklyn South",'68':"PB Brooklyn South",'69':"PB Brooklyn South",'70':"PB Brooklyn South",'71':"PB Brooklyn South",'72':"PB Brooklyn South",'76':"PB Brooklyn South",'78':"PB Brooklyn South",
+  '73': "PB Brooklyn North",'75': "PB Brooklyn North",'77': "PB Brooklyn North",'79': "PB Brooklyn North",'81': "PB Brooklyn North",'83': "PB Brooklyn North",'84': "PB Brooklyn North",'88': "PB Brooklyn North",'90': "PB Brooklyn North",'94': "PB Brooklyn North",
+  '1':"PB Manhattan South",'5':"PB Manhattan South",'6':"PB Manhattan South",'7':"PB Manhattan South",'9':"PB Manhattan South",'10':"PB Manhattan South",'13':"PB Manhattan South",'Midtown':"PB Manhattan South", '17':"PB Manhattan South", 'Midtown':"PB Manhattan South",
+  '19':"PB Manhattan North",'20':"PB Manhattan North", 'Central':"PB Manhattan North", '23':"PB Manhattan North",'24':"PB Manhattan North",'25':"PB Manhattan North",'26':"PB Manhattan North",'28':"PB Manhattan North",'30':"PB Manhattan North",'32':"PB Manhattan North",'33':"PB Manhattan North",'34':"PB Manhattan North",
+  '100':"PB Queens South",'101':"PB Queens South",'102':"PB Queens South",'103':"PB Queens South",'105':"PB Queens South",'106':"PB Queens South",'107':"PB Queens South",'113':"PB Queens South",
+  '104':"PB Queens North",'108':"PB Queens North",'109':"PB Queens North",'110':"PB Queens North",'111':"PB Queens North",'112':"PB Queens North",'114':"PB Queens North",'115':"PB Queens North",
+  '120':"PB Staten Island",'121':"PB Staten Island",'122':"PB Staten Island",'123':"PB Staten Island"}
